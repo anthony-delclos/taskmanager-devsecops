84099da2d4ea7f6593a02efeb91cea7bcf34aed3
anthony-delclos <anthonydelclospro@gmail.com>
2026-04-17 00:23:19 +0200

    feat: add EF Core migrations and MariaDB configuration

--- app/back/TaskManager.API/Program.cs
+++ app/back/TaskManager.API/Program.cs
@@ -1,11 +1,41 @@
+using COPILmatic_back.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Connexion MariaDB
+var connectionString = builder.Configuration.GetConnectionString("Default");
+
+builder.Services.AddDbContext<CopilmaticContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+        policy.WithOrigins(
+            builder.Configuration["Cors:AllowedOrigins"]?.Split(",")
+            ?? new[] { "http://localhost:4200" })
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 var app = builder.Build();
+
+// Migration Automatiques au démarrage
+using (IServiceScope? scope = app.Services.CreateScope())
+{
+    CopilmaticContext? db = scope.ServiceProvider.GetRequiredService<CopilmaticContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,6 +44,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAngular");
 app.UseHttpsRedirection();
 
 var summaries = new[]
@@ -23,7 +54,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -35,7 +66,7 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
-
+app.MapControllers();
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
